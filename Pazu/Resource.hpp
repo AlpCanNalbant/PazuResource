ffb3698095ecdefa1035fc8401c31f8977c9bafc
AlpCanNalbant <enteldantel95@gmail.com>
2024-02-21 12:15:21 +0300

    Johnny Borov's license removed from unassociated file.

--- Pazu/Resource.hpp
+++ Pazu/Resource.hpp
@@ -1,5 +1,4 @@
 // Copyright (c) Alp Can Nalbant. Licensed under the MIT License.
-// Copyright (c) 2024 Johnny Borov <JohnnyBorov@gmail.com>. Released under MIT License.
 
 #pragma once
 #include <string_view>
